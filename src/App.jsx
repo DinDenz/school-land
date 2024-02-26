9ee1dbe9341a715bbb801159079b4da2f5d6dafb
DinDenz <DinDenz@users.noreply.github.com>
2024-02-26 11:35:07 +0300

    beginning

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App-wrapper">
+      <header className="header">
+        Hello. This is the header
+      </header>
+      <main className='main'>
+        this is the main
+      </main>
+      <footer className='footer'>
+        this is the footer
+      </footer>
+    </div>
+  );
+}
+
+export default App;
